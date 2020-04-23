32eae0e6769b5ceba7ef6d8b2b760c11472c7ec6
wangxiaowei010204 <844503316@qq.com>
2020-04-23 20:30:40 +0800

    提交

---
+++ DIDemo/ILearnDI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIDemo
+{
+    public interface ILearnDI
+    {
+        string GetName(string argName);
+    }
+}
---
+++ DIDemo/LearnDI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DIDemo
+{
+    public class LearnDI : ILearnDI
+    {
+        public LearnDI()
+        { 
+        
+        }
+
+        public string GetName(string argName)
+        {
+            return $"我的名字是:{argName}";
+        }
+    }
+}
