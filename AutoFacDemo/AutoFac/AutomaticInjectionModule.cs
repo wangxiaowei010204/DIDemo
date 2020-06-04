9c5a1f1576bed21cef19bf7ef99665717d5a0004
小伟 汪 <844503316@qq.com>
2020-06-04 18:41:12 +0800

    autofac提交

---
+++ AutoFacDemo/AutoFac/AutomaticInjectionModule.cs
@@ -0,0 +1,45 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UserService;
+
+namespace AutoFacDemo.AutoFac
+{
+    /// <summary>
+    /// 自动注入
+    /// </summary>
+    public class AutomaticInjectionModule : Module
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="moduleBuilder"></param>
+        protected override void Load(ContainerBuilder moduleBuilder)
+        {
+            var assemblys = AppDomain.CurrentDomain.GetAssemblies().ToArray();
+            var perRequestType = typeof(IScoped);
+            moduleBuilder.RegisterAssemblyTypes(assemblys)
+                .Where(t => perRequestType.IsAssignableFrom(t) && t != perRequestType)
+                .PropertiesAutowired()
+                .AsImplementedInterfaces()
+                .InstancePerLifetimeScope();
+
+            var perDependencyType = typeof(ITransient);
+            moduleBuilder.RegisterAssemblyTypes(assemblys)
+                .Where(t => perDependencyType.IsAssignableFrom(t) && t != perDependencyType)
+                .PropertiesAutowired()
+                .AsImplementedInterfaces()
+                .InstancePerDependency();
+
+            var singleInstanceType = typeof(ISingleton);
+            moduleBuilder.RegisterAssemblyTypes(assemblys)
+                .Where(t => singleInstanceType.IsAssignableFrom(t) && t != singleInstanceType)
+                .PropertiesAutowired()
+                .AsImplementedInterfaces()
+                .SingleInstance();
+        }
+
+    }
+}
---
+++ AutoFacDemo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Service;
+
+namespace AutoFacDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IUser _user;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IUser user)
+        {
+            _logger = logger;
+            _user = user;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var name = _user.GetUser();
+
+            Console.WriteLine(name);
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
